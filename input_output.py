afb08894749a3e52d33afb9e5450f93d613138d9
SaiChaitanya1916 <saichaitanyavemula@gmail.com>
2025-07-03 23:03:10 +0530

    input statements

--- input_output.py
+++ input_output.py
@@ -1,5 +1,23 @@
-name = input("enter your name:-")
-age = int(input("enter your Age:-"))
+#Example 1
+# name = input("enter your name:-")
+# age = int(input("enter your Age:-"))
 
-print("Your entered Details:- "+name,age)
-print(type(age))
+# print("Your entered Details:- "+name,age,name,age,sep=" , ",end="Here program was ended") #"sep" :- keyword to seperate
+#                                                                                           #"end":- it's keyword used at the end of the program to give any msg
+
+
+#Example 2
+# name = input("Enter your name:-")
+# print("Hello ," +name,"!")
+
+#Example 3
+# num = int(input("Enter the number:-"))
+# print("You entered num:- ",num)         
+
+
+#example 4
+# Pi = float(input("Enter the value of Pi:-"))
+# print("Pi value = ",Pi,sep="")
+
+
+#example 5
