261ef15d05424be5c7142dd7d8c8c2cc62cc1ed6
SaiChaitanya1916 <saichaitanyavemula@gmail.com>
2025-07-06 22:54:25 +0530

    if conditions

---
+++ ifstatement.py
@@ -0,0 +1,8 @@
+condition = input("Enter the weather:")
+
+if condition == "sunny":
+    print("Yes,You can play Cricket")
+elif condition == "rainny":
+    print("It's raining, play indoor games")
+else:
+    print("Play with Robot")
