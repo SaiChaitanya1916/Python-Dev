1f0a2c155313cbe734939c018387b8ca3f2104f4
SaiChaitanya1916 <saichaitanyavemula@gmail.com>
2025-07-07 21:45:19 +0530

    Escape

---
+++ Escape_sequences.py
@@ -0,0 +1,25 @@
+print("Hello\'s world")
+print("Hello\\s world")
+print("Hello\"s world")
+print("Hello\ns world")
+print("Hello\ts world")
+print("Hello\rs world")
+print("Hello\bs world")
+print("Hello\fs world")
+print("Hello\vs world")
+
+
+
+# Escape_Sequences
+
+"""
+\\ :- Backslash
+\' :- Single Quote 
+\" :-Double quote
+\n :-New line
+\t :-Tab
+\r :- carriage return(used for some text file formats)
+\b :- Backspace (movess the cursor back one space)
+\f :- Form Feed(used for some text file formats)
+\v :- vertical Tab (rarely used)
+"""
