32e7d97c020da0f06dacf1542c0f562867dda56f
SaiChaitanya1916 <saichaitanyavemula@gmail.com>
2025-07-07 19:57:40 +0530

    strings

---
+++ strings.py
@@ -0,0 +1,8 @@
+# str = "PYTHON"
+# print(str[-1])
+# print(str[1])
+# print(str[4])
+
+
+str = "CHAITANYA"
+print(str[0:5:2])   #string slicing :- Division of string several parts ,variablename[start:stop:step]
