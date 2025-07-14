641e8a2f273170390adf79baec17fea996d2df47
SaiChaitanya1916 <saichaitanyavemula@gmail.com>
2025-07-14 21:18:46 +0530

    lambda

---
+++ 021-lambda.py
@@ -0,0 +1,11 @@
+# Example 1 Sum of two numbers using lambda function
+
+sum = lambda a,b:a+b
+result = sum(10,4)
+print(result)
+
+# Example 2 Squaring of two numbers using lambda function
+
+squaring = lambda a:a**2
+result = squaring(4)
+print(result)
