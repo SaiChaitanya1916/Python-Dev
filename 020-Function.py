78ebdd91de8e66c899c533759473e9dec315d04c
SaiChaitanya1916 <saichaitanyavemula@gmail.com>
2025-07-14 20:59:51 +0530

    Function

---
+++ 020-Function.py
@@ -0,0 +1,13 @@
+# Example 1
+# def sum(a,b):
+#     return a+b
+# result = sum(5,10)
+# print(result)
+
+# Example 2
+def personal_details(name,age):
+    print("Name:",name)
+    print("Age:",age)
+personal_details(age=22,name="chaitanya")
+
+# Example 3
