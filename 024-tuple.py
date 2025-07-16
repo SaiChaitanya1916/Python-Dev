b01251115d82b96e663833c8edd4a91a55d1429a
SaiChaitanya1916 <saichaitanyavemula@gmail.com>
2025-07-16 21:57:00 +0530

    tuple

---
+++ 024-tuple.py
@@ -0,0 +1,12 @@
+# Tuple : is an ordered,immutable collection of elements in python
+# it is similar to a list,but the main difference is that tuples cannot be changed after creation
+# Onces tuple is created,you can't add,remove,or modify its elements
+
+tuple_name = (1,2,3,2,2)
+
+count = tuple_name.count(2)
+
+index = tuple_name.index(3)
+
+print(count)
+print(index)
